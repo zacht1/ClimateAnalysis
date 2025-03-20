02ed354312ca1c9aaec1ab09347766d009b2ef2c
zach <taylorzachary8@gmail.com>
2025-03-20 11:40:41 -0700

    Update README and add empty script for correlation analysis

---
+++ scripts/correlation_analysis.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 20 00:08:12 2025
+
+@author: zach
+"""
+
+# RESEARCH Q1 : linear correlation between Vancouver temperature and sea level
+# ------------------------------------------------------------------------------------
+# TODO : ryan
+
+
+
+# RESEARCH Q2 : linear correlation between St. John's temperature and sea level
+# ------------------------------------------------------------------------------------
+# TODO : ryan
+
+
+
+
+# RESEARCH Q3 : linear correlation between Vancouver temperature and St John's temperature'
+# ------------------------------------------------------------------------------------
+# TODO : ryan
+
+
+
+
+# RESEARCH Q4 : linear correlation between Vancouver sea level and St John's sea level
+# ------------------------------------------------------------------------------------
+# TODO : zach
+
+
+
+
+# RESEARCH Q5 : linear correlation between Vancouver temperature and ONI
+# ------------------------------------------------------------------------------------
+# TODO : zach
+
+
+
+
+# RESEARCH Q6 : linear correlation between St John's temperature and INO
+# ------------------------------------------------------------------------------------
+# TODO : zach
+
+
