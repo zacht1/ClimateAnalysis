8be015504a38bd10eaf602e84fb5908c32eeffe9
zach <taylorzachary8@gmail.com>
2025-03-25 19:13:05 -0700

    update figures to include annotated params

--- scripts/correlation_analysis.py
+++ scripts/correlation_analysis.py
@@ -7,6 +7,7 @@ Created on Thu Mar 20 00:08:12 2025
 """
 import os
 import seaborn
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -52,14 +53,20 @@ enso_oni = pd.read_csv("./data/ONI_M_1950_2007.csv")
 combined_slev = pd.merge(van_sealvl, sj_sealvl, on="date", suffixes=("_van", "_sj"))
 
 corr_sea_lvl, p_value_sea_lvl = stats.pearsonr(combined_slev["sea_lvl_van"], combined_slev["sea_lvl_sj"])
+sealvl_r2 = corr_sea_lvl ** 2
+
+# linear correlation model
+sealvl_model = stats.linregress(combined_slev["sea_lvl_van"], combined_slev["sea_lvl_sj"])
 
 plt.figure(figsize=(8, 6))
-seaborn.regplot(x=combined_slev["sea_lvl_van"], y=combined_slev["sea_lvl_sj"])
+seaborn.regplot(x=combined_slev["sea_lvl_van"], y=combined_slev["sea_lvl_sj"], line_kws={"color": "red", "lw":1})
+plt.annotate(f'$R^2$: {np.round(sealvl_r2, 3)}\n P-value: {p_value_sea_lvl:.2e}', xy=(0.05, 0.85), xycoords='axes fraction', color='k', fontsize=12)
+plt.annotate(f'y = {np.round(sealvl_model[0],4)}x + {np.round(sealvl_model[1],4)}', xy=(0.65, 0.05), xycoords='axes fraction', color='r', fontsize=12)
 plt.xlabel("Vancouver Sea Level (m)")
 plt.ylabel("St. John's Sea Level (m)")
 plt.title("Correlation between Vancouver and St. John's Sea Levels")
 # plt.grid(True)
-plt.savefig('van_sj_sealvl_corr.png')
+plt.savefig('figures/van_sj_sealvl_corr.png')
 plt.show()
 
 
@@ -75,14 +82,22 @@ enso_oni["date"] = pd.to_datetime(enso_oni["date"])
 van_temp["year_month"] = van_temp["date"].dt.to_period("M")
 enso_oni["year_month"] = enso_oni["date"].dt.to_period("M")
 
-combined_data = pd.merge(van_temp, enso_oni, on="year_month", suffixes=("_temp", "_oni"))
+combined_data = pd.merge(van_temp, enso_oni, on="year_month", suffixes=("_temp", "_oni")).dropna(subset=['mean_temp', 'oni'])
+
+corr, p_value = stats.pearsonr(combined_data["mean_temp"], combined_data["oni"])
+r_squared = corr ** 2
+
+# linear correlation model
+model = stats.linregress(combined_data["mean_temp"], combined_data["oni"])
 
 plt.figure(figsize=(8, 6))
-seaborn.regplot(x=combined_data["mean_temp"], y=combined_data["oni"])
+seaborn.regplot(x=combined_data["mean_temp"], y=combined_data["oni"], line_kws={"color": "red", "lw":1})
+plt.annotate(f'$R^2$: {np.round(r_squared, 3)}\n P-value: {p_value:.2e}', xy=(0.05, 0.85), xycoords='axes fraction', color='k', fontsize=12)
+plt.annotate(f'y = {np.round(model[0],4)}x + {np.round(model[1],4)}', xy=(0.65, 0.05), xycoords='axes fraction', color='r', fontsize=12)
 plt.xlabel("Vancouver Temperature (ºC)")
 plt.ylabel("Oceanic Nino Index")
 plt.title("Correlation between Vancouver Air Temperature and Oceanic Nino Index")
-plt.savefig('vantemp_oni_corr.png')
+plt.savefig('figures/vantemp_oni_corr.png')
 plt.show()
 
 
@@ -94,14 +109,22 @@ plt.show()
 sj_temp["date"] = pd.to_datetime(sj_temp["date"])
 sj_temp["year_month"] = sj_temp["date"].dt.to_period("M")
 
-combined_data = pd.merge(sj_temp, enso_oni, on="year_month", suffixes=("_temp", "_oni"))
+combined_data = pd.merge(sj_temp, enso_oni, on="year_month", suffixes=("_temp", "_oni")).dropna(subset=['mean_temp', 'oni'])
+
+corr, p_value = stats.pearsonr(combined_data["mean_temp"], combined_data["oni"])
+r_squared = corr ** 2
+
+# linear correlation model
+model = stats.linregress(combined_data["mean_temp"], combined_data["oni"])
 
 plt.figure(figsize=(8, 6))
-seaborn.regplot(x=combined_data["mean_temp"], y=combined_data["oni"])
+seaborn.regplot(x=combined_data["mean_temp"], y=combined_data["oni"], line_kws={"color": "red", "lw":1})
+plt.annotate(f'$R^2$: {np.round(r_squared, 3)}\n P-value: {p_value:.2e}', xy=(0.05, 0.85), xycoords='axes fraction', color='k', fontsize=12)
+plt.annotate(f'y = {np.round(model[0],4)}x + {np.round(model[1],4)}', xy=(0.65, 0.05), xycoords='axes fraction', color='r', fontsize=12)
 plt.xlabel("St. John's Temperature (ºC)")
 plt.ylabel("Oceanic Nino Index")
 plt.title("Correlation between St John's Air Temperature and Oceanic Nino Index")
-plt.savefig('sjtemp_oni_corr.png')
+plt.savefig('figures/sjtemp_oni_corr.png')
 plt.show()
 
 
